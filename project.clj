cad71eec291054853e76c539469e81f2ec1296d2
Edward Chan <esh@mini-esh.(none)>
2011-01-15 17:50:17 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject intrade-marketmaker "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+								 [intrade-tradeservice "1.0.0-SNAPSHOT"]])
---
+++ test/intrade_marketmaker/test/core.clj
@@ -0,0 +1,6 @@
+(ns intrade-marketmaker.test.core
+  (:use [intrade-marketmaker.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
